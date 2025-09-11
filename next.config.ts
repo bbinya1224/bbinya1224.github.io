f44403ee40fd71e367148090b76ac6948c680b3f
subinLee <salllena33@gmail.com>
2025-09-11 23:08:43 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 정적 사이트 생성
+  images: {
+    unoptimized: true, // 이미지 최적화 끄기
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
 };
 
 export default nextConfig;
