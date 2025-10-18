bbc23ec9b0ad568f7df063ee8f7ac128c4ca2710
bbinya1224 <bbinya1224@gmail.com>
2025-10-18 22:16:40 +0900

    style: Badge component transition only shadow

--- src/shared/ui/Badge.tsx
+++ src/shared/ui/Badge.tsx
@@ -8,7 +8,7 @@ const Badge = ({ children, className, ...props }: BadgeProps) => {
   return (
     <span
       className={cn(
-        "inline-flex cursor-default items-center justify-center rounded-full px-1 py-0.5 text-sm font-medium whitespace-nowrap transition-all duration-200 hover:shadow-md sm:px-2 sm:text-base",
+        "inline-flex cursor-default items-center justify-center rounded-full px-1 py-0.5 text-sm font-medium whitespace-nowrap transition-[shadow] duration-200 hover:shadow-md sm:px-2 sm:text-base",
         className,
       )}
       {...props}
