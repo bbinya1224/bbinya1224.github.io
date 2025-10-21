e26349234f8b97085e1cdf242d0e1b39da8ac824
bbinya1224 <bbinya1224@gmail.com>
2025-10-21 20:09:46 +0900

    chore: google Search Console 에 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,14 @@ import "@/app/style/globals.css";
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
+      <head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="lmWHNnNVPVnXS1B-FC4sGMw-0mI1J4yb10kl4OZxTOY"
+        />
+      </head>
+
       <body>
         <main>{children}</main>
       </body>
@@ -15,6 +23,9 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
 export const metadata: Metadata = {
   title: "삔아's Blog",
   description: "프론트엔드 개발자 삔아 기술 블로그",
+  verification: {
+    google: "lmWHNnNVPVnXS1B-FC4sGMw-0mI1J4yb10kl4OZxTOY",
+  },
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,15 @@ module.exports = {
   generateRobotsTxt: true,
   outDir: "out",
   sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 1,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/exclude"],
+      },
+    ],
+  },
 };
